21b529065322da26502dfbd2de60e2078b3f32ef
Robert Middleton <robert.middleton@rm5248.com>
2021-05-10 18:38:48 -0400

    Packaging for the simple application and library

---
+++ library-package/simple-lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "simple-lib.h"
+
+void simple_lib_function(){
+	printf( "Simple lib function!\n" );
+}
---
+++ library-package/simple-lib.h
@@ -0,0 +1,6 @@
+#ifndef SIMPLE_LIB_H
+#define SIMPLE_LIB_H
+
+void simple_lib_function(void);
+
+#endif /* SIMPLE_LIB_H */
